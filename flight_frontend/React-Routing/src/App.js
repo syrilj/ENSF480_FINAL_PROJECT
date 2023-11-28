a882dc187758a7b5a284a5e213403f54493f3ee1
Rodmichael <rodmichael.umapas@ucalgary.ca>
2023-11-28 14:02:38 -0700

    update to 3 pages from home

--- flight_frontend/React-Routing/src/App.js
+++ flight_frontend/React-Routing/src/App.js
@@ -2,7 +2,7 @@ import {Routes, Route} from "react-router-dom";
 
 
 import Signup from "./component/Signup";
-
+import Home from "./component/Home";
 import Navbar from "./component/Navbar";
 import Login from "./component/Login";
 
@@ -13,12 +13,12 @@ function App() {
     <Navbar />
 
     <Routes >
+      <Route path="/" element={ <Home /> } />
       
       
       
-      
-      <Route path="/" element={ <Login /> } />
-      <Route path="/Signup" element={ <Signup /> } />
+      <Route path="/Login" element={ <Login /> } />
+      <Route path="/Signupt" element={ <Signup /> } />
     </Routes>
     </>
   );
