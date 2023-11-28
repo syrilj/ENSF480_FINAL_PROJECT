16038213de1c1d487bcc9458c965468fc22a7d01
Rodmichael <rodmichael.umapas@ucalgary.ca>
2023-11-28 15:46:49 -0700

    update login styling

--- flight_frontend/React-Routing/src/component/Login.js
+++ flight_frontend/React-Routing/src/component/Login.js
@@ -35,19 +35,21 @@ const LoginForm = () => {
     };
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <h2 className="text-center m-4">Login</h2>
+        <section className="flex items-center justify-center h-screen">
+            <div className="text-center">
+                <h2 className="text-3xl font-semibold text-gray-800 capitalize dark:text-white lg:text-4xl">
+                    Login
+                </h2>
 
-                    <form onSubmit={(e) => onSubmit(e)}>
+                <div className="mt-8 w-full max-w-md mx-auto">
+                    <form onSubmit={(e) => onSubmit(e)} className="px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:shadow-black/50">
                         <div className="mb-3">
-                            <label htmlFor="Username" className="form-label">
+                            <label htmlFor="Username" className="block mb-2 text-sm text-black dark:text-black">
                                 Username
                             </label>
                             <input
                                 type={"text"}
-                                className="form-control"
+                                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                                 placeholder="Enter your username"
                                 name="u_username"
                                 value={u_username}
@@ -55,12 +57,12 @@ const LoginForm = () => {
                             />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="Password" className="form-label">
+                            <label htmlFor="Password" className="block mb-2 text-sm text-black dark:text-black">
                                 Password
                             </label>
                             <input
                                 type={"password"}
-                                className="form-control"
+                                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                                 placeholder="Enter your password"
                                 name="u_password"
                                 value={u_password}
@@ -68,16 +70,28 @@ const LoginForm = () => {
                             />
                         </div>
                         {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
-                        <button type="submit" className="btn btn-outline-primary">
+                        <button
+                            type="submit"
+                            className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                        >
                             Login
                         </button>
-                        <Link className="btn btn-outline-danger mx-2" to="/">
+                        <Link
+                            className="w-full block px-6 py-3 mt-2 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md dark:text-gray-300 hover:border-blue-400 dark:hover:border-blue-400 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            to="/"
+                        >
                             Cancel
+                        </Link>
+                        <Link
+                            className="w-full block px-6 py-3 mt-2 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md dark:text-gray-300 hover:border-blue-400 dark:hover:border-blue-400 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                            to="/Signup"
+                        >
+                            Sign Up
                         </Link>
                     </form>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
