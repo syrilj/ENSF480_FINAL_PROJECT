4534d5e7899a421a8c03a7ee3461d750ba95ecf3
syrilj <83786796+syrilj@users.noreply.github.com>
2023-11-28 15:11:53 -0700

    got signup and login done

--- flight_frontend/React-Routing/src/App.js
+++ flight_frontend/React-Routing/src/App.js
@@ -1,7 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 
 
-import Signup from "./component/Signup";
+import SignUpForm from "./component/Signup";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
 import Login from "./component/Login";
@@ -18,7 +18,7 @@ function App() {
       
       
       <Route path="/Login" element={ <Login /> } />
-      <Route path="/Signup" element={ <Signup /> } />
+      <Route path="/Signup" element={ <SignUpForm /> } />
     </Routes>
     </>
   );
--- flight_frontend/React-Routing/src/component/Signup.js
+++ flight_frontend/React-Routing/src/component/Signup.js
@@ -1,165 +1,81 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-function Signup() {
-    /*const [formData, setFormData] = useState({
-      name: '',
-      gender: '',
-      address: '',
-      email: '',
-      contact: '',
-      username: '',
-      password: '',
-    });
-  
-    const handleChange = (e) => {
-      setFormData({ ...formData, [e.target.id]: e.target.value });
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-        const response = await fetch('http://your-backend-url/api/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        });
-  
-        if (response.ok) {
-          // Registration successful
-          console.log('User registered successfully');
-          // Redirect or perform any other actions as needed
-        } else {
-          // Registration failed
-          console.error('Failed to register user');
-        }
-      } catch (error) {
-        console.error('Error during registration:', error);
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    u_name: "",
+    u_gender: "",
+    u_address: "",
+    u_email_id: "",
+    u_contact: "",
+    u_username: "",
+    u_password: "",
+  });
+
+  const [successMessage, setSuccessMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const onInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8081/api/user/user_register", formData);
+
+      if (response.status === 200) {
+        setSuccessMessage("Your registration is successful. Use your credentials for login!");
+        setError("");
+      } else {
+        setSuccessMessage("");
+        setError("Registration failed. Please try again.");
       }
-    };
-*/
+    } catch (error) {
+      setSuccessMessage("");
+      setError("Error during registration. Please try again.");
+      console.error("Error during registration:", error);
+    }
+  };
 
   return (
-    <section className="flex items-center justify-center h-screen">
-      <div className="text-center">
-        <h1 className="text-3xl font-semibold text-gray-800 capitalize dark:text-white lg:text-4xl">
-          Sign Up for 480 Flight Assistance
-        </h1>
-
-        <div className="mt-8 w-full max-w-md mx-auto">
-          <form className="px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:shadow-black/50">
-            <div className="mb-4">
-              <label htmlFor="name" className="block mb-2 text-sm text-black dark:text-black">
-                Full Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                placeholder="John Doe"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="username" className="block mb-2 text-sm text-black dark:text-black">
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                placeholder="johndoe123"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="email" className="block mb-2 text-sm text-black dark:text-black">
-                Email address
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="johndoe@example.com"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="password" className="block mb-2 text-sm text-black dark:text-black">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="********"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="address" className="block mb-2 text-sm text-black dark:text-black">
-                Address
-              </label>
-              <input
-                type="text"
-                id="address"
-                placeholder="123 Main St, City, Country"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="contact" className="block mb-2 text-sm text-black dark:text-black">
-                Contact Number
-              </label>
-              <input
-                type="tel"
-                id="contact"
-                placeholder="(555) 123-4567"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="gender" className="block mb-2 text-sm text-black dark:text-black">
-                Gender
-              </label>
-              <select
-                id="gender"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              >
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="userType" className="block mb-2 text-sm text-black dark:text-black">
-                User Type
-              </label>
-              <select
-                id="userType"
-                className="w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-black-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              >
-                <option value="registered">Registered User</option>
-                <option value="admin">Admin</option>
-              </select>
-            </div>
-
-            <button
-              type="submit"
-              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-            >
-              Sign Up
-            </button>
-          </form>
-        </div>
+      <div>
+        <h2>Sign Up</h2>
+        <form onSubmit={onSubmit}>
+          <div>
+            <label>Name:</label>
+            <input type="text" name="u_name" value={formData.u_name} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label>Gender:</label>
+            <input type="text" name="u_gender" value={formData.u_gender} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label>Address:</label>
+            <input type="text" name="u_address" value={formData.u_address} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" name="u_email_id" value={formData.u_email_id} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label>Contact:</label>
+            <input type="text" name="u_contact" value={formData.u_contact} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label>Username:</label>
+            <input type="text" name="u_username" value={formData.u_username} onChange={onInputChange} required />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" name="u_password" value={formData.u_password} onChange={onInputChange} required />
+          </div>
+          {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <button type="submit">Sign Up</button>
+        </form>
       </div>
-    </section>
   );
-}
+};
 
-export default Signup;
+export default SignUpForm;
